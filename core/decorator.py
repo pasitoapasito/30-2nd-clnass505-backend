3b17e843f1f2c3c13a253123e53b4e5e6a9dbabd
DGK <rlaehdrb0923@gmail.com>
2022-03-18 15:42:18 +0900

    Complete : lectures app LectureDetailView and LectureDetail tests.py

--- core/decorator.py
+++ core/decorator.py
@@ -1,5 +1,6 @@
-import jwt
+import jwt, time, functools
 
+from django.db    import connection, reset_queries
 from django.conf  import settings
 from django.http  import JsonResponse
 
@@ -21,4 +22,43 @@ def signin_decorator(func):
 
         return func(self, request, *args, **kwargs)
 
+    return wrapper
+
+def public_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            request.user = None
+            
+            if access_token:
+                payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+                user         = User.objects.get(id=payload['user_id'])
+                request.user = user
+        
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=401)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
+
+def query_debugger(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        reset_queries()
+        number_of_start_queries = len(connection.queries)
+        start  = time.perf_counter()
+        result = func(*args, **kwargs)
+        end    = time.perf_counter()
+        number_of_end_queries = len(connection.queries) - 2 if len(connection.queries) else 0
+        print(f"-------------------------------------------------------------------")
+        print(f"Function : {func.__name__}")
+        print(f"Number of Queries : {number_of_end_queries-number_of_start_queries}")
+        print(f"Finished in : {(end - start):.2f}s")
+        print(f"-------------------------------------------------------------------")
+        return result
+    
     return wrapper
--- lectures/views.py
+++ lectures/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Avg
 
-# Create your views here.
+from lectures.models  import Lecture
+from users.models     import UserLecture
+from core.decorator   import public_decorator
+
+class LectureDetailView(View):
+    @public_decorator
+    def get(self, request, lecture_id):
+        try:
+            user    = request.user
+            lecture = Lecture.objects\
+                             .select_related('user', 'subcategory')\
+                             .prefetch_related('lectureimage_set', 'review_set', 'like_set')\
+                             .get(id=lecture_id)
+                
+            if UserLecture.objects.filter(user=user, lecture=lecture).exists():
+                user_status = 'student'
+                
+            if not UserLecture.objects.filter(user=user, lecture=lecture).exists():
+                user_status = 'potential_student'
+                
+            if lecture.user == user:
+                user_status = 'creator'
+            
+            if user is None:
+                user_status = None
+            
+            result = {
+                'id'                        : lecture.id,
+                'subcategory'               : lecture.subcategory.name,
+                'creator_nickname'          : lecture.user.nickname,
+                'creator_profile_image_url' : lecture.user.profile_image_url,
+                'title'                     : lecture.name,
+                'price'                     : lecture.price,
+                'discount_rate'             : lecture.discount_rate if lecture.discount_rate else None,
+                'discount_price'            : float(lecture.price) * (1-(lecture.discount_rate/100))\
+                                              if lecture.discount_rate else None,
+                'description'               : lecture.description,
+                'difficulty'                : lecture.difficulty.name,
+                'review_avg_rating'         : round(lecture.review_set.all().aggregate(Avg('rating'))['rating__avg'], 1)\
+                                              if lecture.review_set.all() else None,
+                'likes'                     : lecture.like_set.count(),
+                'thumbnail_image_url'       : lecture.thumbnail_image_url,
+                'detail_image_url'          : {image.sequence : image.image_url for image in lecture.lectureimage_set.all()},
+                'user_status'               : user_status, 
+                'reviews_info' : [
+                    {
+                        'user'      : review.user.name,
+                        'title'     : review.title,
+                        'content'   : review.content,
+                        'rating'    : review.rating,
+                        'image_url' : [image.image_url for image in review.reviewimage_set.all()]
+                    } for review in lecture.review_set.all()],    
+            }
+            
+            return JsonResponse({'message' : 'SUCCESS', 'result' : result}, status=200)
+        
+        except Lecture.DoesNotExist:
+            return JsonResponse({'message' : 'LECTURE_NOT_EXIST'}, status=400)
