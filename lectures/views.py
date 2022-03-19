41af957b1451561b8a826296d79c38e6be86d8f2
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-19 17:59:37 +0900

    add:lecture,lectureimage,user_creator

--- lectures/views.py
+++ lectures/views.py
@@ -1,15 +1,13 @@
 import boto3
-import json
 
-
-from uuid import uuid4
-
-
-from django.http   import JsonResponse, HttpResponse
+from uuid          import uuid4
+from django.http   import JsonResponse
 from django.views  import View
 
-from my_settings     import AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY
+from users.models    import User
 from lectures.models import Lecture, LectureImage
+from core.decorator  import signin_decorator
+from my_settings     import AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY
 
 class FileView(View):
     s3_client = boto3.client(
@@ -17,16 +15,41 @@ class FileView(View):
         aws_access_key_id     = AWS_ACCESS_KEY_ID,
         aws_secret_access_key = AWS_SECRET_ACCESS_KEY
     )
-    
+    @signin_decorator
     def post(self, request):
         try:
-
-            file = request.FILES.getlist['filename']
-            BUCKET_ADDRESS = 'https://woosbucket.s3.ap-northeast-2.amazonaws.com'
-            BUCKET_DIRECTORY = 'image'
+            profile  = request.FILES['profile']
+            user     = request.user
+           
+            users    = User.objects.filter(id=user.id)
+            user_ids = User.objects.get(id=user.id)
+            BUCKET_ADDRESS    = 'https://woosbucket.s3.ap-northeast-2.amazonaws.com'
+            BUCKET_DIRECTORY  = 'thumnail'
+            BUCKET_DIRECTORY2 = 'image'
+            BUCKET_DIRECTORY3 = 'profile'
+            url          = f"{BUCKET_DIRECTORY}/{str(uuid4())}"
+            urls         = f"{BUCKET_ADDRESS}/{url}"
+            profile_url  = f"{BUCKET_DIRECTORY3}/{str(uuid4())}"
+            profile_urls = f"{BUCKET_ADDRESS}/{profile_url}"
             
-            url =f"{BUCKET_DIRECTORY}/{str(uuid4())}"
-            urls =f"{BUCKET_ADDRESS}/{url}"
+            self.s3_client.upload_fileobj(
+                profile, 
+                "woosbucket",
+                profile_url,
+                ExtraArgs={
+                    "ContentType" : profile.content_type
+                }
+            )
+           
+            users.update(
+                profile_image_url = profile_urls,
+                nickname          = request.POST['nickname'],
+                description       = request.POST['introduce']
+            )
+            
+            file    = request.FILES['thumnail']
+            files   = request.FILES.getlist('image')
+    
             self.s3_client.upload_fileobj(
                 file, 
                 "woosbucket",
@@ -36,69 +59,38 @@ class FileView(View):
                 }
             )
             
-            
-            #lecture, is_created = 
-            Lecture.objects.create(
-                name                = request.POST['name'],
-                price               = request.POST['price'],
-                discount_rate       = request.POST['discount_rate'],
-                thumbnail_image_url = urls,
-                description         = request.POST['description'],
-                user_id             = request.POST['user_id'],
-                difficulty_id       = request.POST['difficulty_id'],
-                subcategory_id      = request.POST['subcategory_id']
+            lecture = Lecture.objects.create(
+                    name                = request.POST['name'],
+                    price               = request.POST['price'],
+                    discount_rate       = request.POST['discount_rate'],
+                    thumbnail_image_url = urls,
+                    description         = request.POST['description'],
+                    user_id             = user.id,
+                    difficulty_id       = request.POST['difficulty_id'],
+                    subcategory_id      = request.POST['subcategory_id']
             )
-            # if is_created:
+            
+            for i in range(3):
+                image_url  = f"{BUCKET_DIRECTORY2}/{str(uuid4())}"
+                image_urls = f"{BUCKET_ADDRESS}/{image_url}"
                 
-            #     self.s3_client.upload_fileobj(
-            #     image, 
-            #     "woosbucket",
-            #     url,
-                
-            #     ExtraArgs={
-            #         "ContentType": file.content_type
-            #     }
-            # )
-                
-                
-            #     LectureImage.objects.create(
-            #         title      = request.POST['title'],
-            #         image_url  = urls,
-            #         sequence   = request.POST['sequence'],
-            #         lecture_id = lecture.id
-                    
-            #     )
+                self.s3_client.upload_fileobj(
+                    files[i], 
+                    "woosbucket",
+                    image_url,
+                    ExtraArgs={
+                        "ContentType": file.content_type
+                    }
+                )
+            
+                LectureImage.objects.create(
+                        title      = request.POST['title'],
+                        image_url  = image_urls,
+                        sequence   = i,
+                        lecture_id = lecture.id
+                        
+                    )
             return JsonResponse({"message" : "success"},status=201)        
-        
         
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"},status=400)
-        
-        
-        # file   = request.FILES['filename']
-        # data = request.POST
-        # # print(data)
-       
-        # BUCKET_ADDRESS = 'https://woosbucket.s3.ap-northeast-2.amazonaws.com'
-        # BUCKET_DIRECTORY = 'image'
-        # self.s3_client.upload_fileobj(
-        #     file, 
-        #     "woosbucket",
-        #     f"{BUCKET_DIRECTORY}/{uuid4()}",
-        #     ExtraArgs={
-        #         "ContentType": file.content_type
-        #     }
-        # ) 
-
-        # return HttpResponse(status= 200)
-        
-              
-
-        """
-        목적: 수업을 등록
-        
-        1. 수업에 대한 정보를 디비에 저장
-        2. 이미지 저장(어떤 수업의 이미지인지 알아야됨) 저 주소를 조합해서 db에 저장하면 됩니다.
-        
-        
-        """
