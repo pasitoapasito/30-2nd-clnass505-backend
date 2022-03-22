c47343072b19c828845fb9b6e55e393839c65afa
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-22 15:24:05 +0900

    mentor review

---
+++ core/storage.py
@@ -0,0 +1,49 @@
+import boto3
+import uuid
+
+from clnass505.settings import AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET_NAME
+
+class MyS3Client:
+    def __init__(self, access_key, secret_key, bucket_name):
+        boto3_s3 = boto3.client(
+            's3',
+            aws_access_key_id     = access_key,
+            aws_secret_access_key = secret_key
+        )
+        self.s3_client   = boto3_s3
+        self.bucket_name = bucket_name
+
+    def upload(self, file):
+        try: 
+            file_id    = str(uuid.uuid4())
+            extra_args = { 'ContentType' : file.content_type }
+
+            self.s3_client.upload_fileobj(
+                    file,
+                    self.bucket_name,
+                    file_id,
+                    ExtraArgs = extra_args
+                )
+            return f'https://{self.bucket_name}.s3.ap-northeast-2.amazonaws.com/{file_id}'
+        except:
+            return None
+
+    def delete(self, file_name):
+        return self.s3_client.delete_object(bucket=self.bucket_name, Key=f'{file_name}')
+
+class MyGoogleClient:
+    def __init__(self):
+        pass
+    
+    def upload(self, file):
+        pass
+
+s3_client = MyS3Client(AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET_NAME)
+google_client = MyGoogleClient()
+
+class FileUpload:
+    def __init__(self, client):
+        self.client = client
+    
+    def upload(self, file):
+        return self.client.upload(file)
--- lectures/views.py
+++ lectures/views.py
@@ -10,14 +10,9 @@ from users.models    import User
 from lectures.models import Lecture, LectureImage
 from core.decorator  import signin_decorator
 from my_settings     import AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,BUCKET_ADDRESS,BUCKET_DIR_THUMNAIL,BUCKET_DIR_IMAGE,BUCKET_DIR_PROFILE 
+from core.storage import s3_client, FileUpload, google_client
 
-class LecturesView(View):
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id     = AWS_ACCESS_KEY_ID,
-        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
-    )
-    
+class LecturesView(View):    
     @signin_decorator
     def post(self, request):
         try:
@@ -38,32 +33,18 @@ class LecturesView(View):
             subcategory_id = request.POST['subcategory_id']
             title          = request.POST['title']
 
-            
+            file_handler = FileUpload(google_client)
 
             with transaction.atomic():
                 thumnail_url        = f"{BUCKET_DIR_THUMNAIL}/{str(uuid4())}"
                 profile_image_url   = f"{BUCKET_DIR_PROFILE}/{str(uuid4())}"
                 
-                self.s3_client.upload_fileobj(
-                    profile, 
-                    "woosbucket",
-                    profile_image_url,
-                    ExtraArgs={
-                        "ContentType" : profile.content_type
-                    }
-                )
+                file_handler.upload(profile)
             
                 user.profile_image_url = f"{BUCKET_ADDRESS}/{profile_image_url}"
                 user.save()
               
-                self.s3_client.upload_fileobj(
-                    thumnail, 
-                    "woosbucket",
-                    thumnail_url,
-                    ExtraArgs={
-                        "ContentType": thumnail.content_type
-                    }
-                )
+                file_handler.upload(thumnail)
                 
                 lecture = Lecture.objects.create(
                         name                = name,
@@ -76,24 +57,21 @@ class LecturesView(View):
                         subcategory_id      = subcategory_id
                 )
                 
+                lecture_images = []
+
                 for idx, lecture_image in enumerate(lecture_images):
                     image_url  = f"{BUCKET_DIR_IMAGE}/{str(uuid4())}"
                     
-                    self.s3_client.upload_fileobj(
-                        lecture_image, 
-                        "woosbucket",
-                        image_url,
-                        ExtraArgs={
-                            "ContentType": thumnail.content_type
-                        }
-                    )
+                    file_handler.upload(lecture_image)
                 
-                    LectureImage.objects.create(
-                            title      = title,
-                            image_url  = f"{BUCKET_ADDRESS}/{image_url}",
-                            sequence   = idx + 1,
-                            lecture_id = lecture.id
-                    )       
+                    lecture_images.append(LectureImage(
+                        title      = title,
+                        image_url  = f"{BUCKET_ADDRESS}/{image_url}",
+                        sequence   = idx + 1,
+                        lecture_id = lecture.id
+                    ))
+
+                LectureImage.objects.bulk_create(lecture_images)       
                  
             return JsonResponse({"message" : "success"},status=201)        
         
