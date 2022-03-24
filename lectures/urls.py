fa9e9e46ea065bec14956c3972ef6da90fc81f18
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-24 13:31:29 +0900

    modify: url path

--- lectures/urls.py
+++ lectures/urls.py
@@ -4,5 +4,5 @@ from lectures.views import LectureDetailView, LectureLikeView,LecturesView
 urlpatterns = [
     path('/<int:lecture_id>', LectureDetailView.as_view()),
     path('/<int:lecture_id>/like', LectureLikeView.as_view()),
-    path('/upload',LecturesView.as_view()),
+    path('',LecturesView.as_view()),
 ]
